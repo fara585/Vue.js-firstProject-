1f95e85c2c808d9e26b04534b12379191a5e1912
fara585 <sa1dakbarof@gmail.com>
2024-05-06 16:45:51 +0500

    first commit

---
+++ src/components/Foooter.vue
@@ -0,0 +1,168 @@
+<template>
+    <footer class="footer">
+        <div class="container">
+            <div class="footer__content">
+
+                <div class="footer__content-left">
+
+                    <ul class="footer__left-list">
+                        <li><a href="" class="footer__list-items">О компании</a></li>
+                        <li><a href="" class="footer__list-items">Партнеры</a></li>
+                        <li><a href="" class="footer__list-items">Новости</a></li>
+                        <li><a href="" class="footer__list-items">Документация / Регламенты</a></li>
+                        <li><a href="" class="footer__list-items">Вход</a></li>
+                        <li><a href="" class="footer__list-items">Регистрация</a></li>
+                        <li><a href="" class="footer__list-items">Проверка транспортного средства</a></li>
+                    </ul>
+
+                    <div class="footer__left-payment">
+                        <img src="../assets/img/visa.png" alt="">
+                        <img src="../assets/img/master.png" alt="">
+                        <img src="../assets/img/mir-logo.png" alt="">
+                    </div>
+
+                    <p>2009 - 2021 © Все права защищены.</p>
+
+                </div>
+
+                <div class="footer__content-right">
+
+                    <div class="footer__right-top">
+                        <div class="footer__top-box">
+                            <h3>{{ contact.number }}</h3>
+                            <p>Звонок по России бесплатный</p>
+                        </div>
+                        <h3>{{ contact.gmail }}</h3>
+                        <h3>{{ contact.email }}</h3>
+                    </div>
+
+                    <div class="footer__right-social">
+                        <img src="../assets/img/vk.png" alt="">
+                        <img src="../assets/img/facebook.png" alt="">
+                        <img src="../assets/img/insta.png" alt="">
+                        <img src="../assets/img/youtube.png" alt="">
+                        <p>Мы в соцсетях</p>
+                    </div>
+
+                    <p class="footer__right-p">{{ contact.about }}</p>
+
+                </div>
+
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            contact: {
+                number: '8 800 550-26-65',
+                gmail: 'assist@sd-assist.ru',
+                email: 'auction@sd-assist.ru ',
+                about: 'Свидетельство о регистрации электронного СМИ ЭЛ № ФС 77 - 76018 выдано Федеральной службой по надзору в сфере связи, информационных технологий и массовых коммуникаций 24.06.2019 года',
+            }
+        }
+    },
+}
+</script>
+
+<style>
+.footer {
+    background: #F2F8FF;
+    border-radius: 50px 50px 0 0;
+    margin-top: -50px;
+}
+
+.footer__content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 55px 0;
+}
+
+.footer__left-list {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
+
+.footer__list-items {
+    display: block;
+    color: #163C66;
+    font-family: "GothamProMedium";
+    transition: 0.5s;
+}
+
+
+.footer__list-items:hover {
+    transform: translateX(12px);
+}
+
+.footer__left-payment {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    padding: 20px 0 30px;
+}
+
+.footer__content-left p {
+    color: #163C66;
+    font-family: "GothamProMedium";
+}
+
+.footer__content-right {
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+}
+
+.footer__right-top {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
+
+.footer__right-top h3 {
+    color: #163C66;
+    font-size: 32px;
+    line-height: 38.4px;
+    font-family: "GothamProMedium";
+}
+
+.footer__top-box {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+}
+
+.footer__top-box p {
+    color: #163C66;
+    font-size: 12px;
+    left: 14.4px;
+    font-family: "GothamProMedium";
+}
+
+.footer__right-social {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+}
+
+.footer__right-social img{
+    cursor: pointer;
+}
+
+.footer__right-social p {
+    color: #163C66;
+    font-family: "GothamProMedium";
+}
+
+.footer__right-p {
+    width: 580px;
+    color: #163C66;
+    line-height: 19px;
+
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,153 @@
+<template>
+    <nav class="nav">
+        <div class="container">
+            <div class="nav__content">
+
+                <div class="hamburger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+
+                <div class="nav__content-block">
+                    <img src="../assets/img/nav__title.svg" alt="">
+                    <div class="nav__block-txt">
+                        <p>В партнерстве с</p>
+                        <img src="../assets/img/logo-bottom.png" alt="">
+                    </div>
+                </div>
+
+                <h3 class="nav__content-title">Проведено торгов: <span>{{ sold }}</span> </h3>
+
+                <form class="nav__content-frm">
+                    <div class="nav__frm-search">
+                        <img src="../assets/img/search.svg" alt="">
+                        <input type="search" placeholder="Поиск по лотам" v-model="search">
+                    </div>
+                    <button class="nav__frm-btm">Личный кабинет</button>
+                </form>
+                
+            </div>
+        </div>
+    </nav>
+    <Header />
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+
+
+export default {
+    components: {
+        Header
+    },
+    data() {
+        return {
+            sold: '12 345',
+            search: '',
+        }
+    },
+}
+</script>
+
+<style>
+.nav {
+    border: 1px solid #1C426D
+}
+
+.nav__content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 25px 0;
+}
+
+.hamburger {
+    position: relative;
+    width: 35px;
+    height: 18px;
+    cursor: pointer;
+}
+
+.hamburger span {
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    background: #163C66;
+}
+
+.hamburger span:nth-child(3) {
+    position: absolute;
+    bottom: 0;
+}
+
+.hamburger span:nth-child(2) {
+    position: absolute;
+    top: 8px;
+}
+
+.nav__content-block {
+    display: flex;
+    flex-direction: column;
+    gap: 14px;
+}
+
+.nav__block-txt {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+}
+
+.nav__block-txt p {
+    font-size: 12px;
+    color: #21212666;
+
+}
+
+.nav__content-title {
+    color: #8C9091;
+    font-size: 14px;
+    left: 13.4px;
+}
+
+.nav__content-title span {
+    color: #163C66;
+    font-family: "GothamProBold";
+    font-size: 16px;
+}
+
+.nav__content-frm {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+}
+
+.nav__frm-search {
+    display: flex;
+    align-items: center;
+    gap: 15px;
+    padding: 13px 20px;
+    border: 1px solid #1C426D33;
+    border-radius: 6px;
+}
+
+.nav__content-frm input {
+    outline: none;
+    border: none;
+    color: #8C9091;
+}
+
+.nav__frm-btm {
+    border: none;
+    outline: none;
+    background: #1C426D;
+    color: white;
+    left: 19.2px;
+    text-align: center;
+    padding: 14px 22.5px;
+    border-radius: 6px;
+    cursor: pointer;
+}
+</style>
